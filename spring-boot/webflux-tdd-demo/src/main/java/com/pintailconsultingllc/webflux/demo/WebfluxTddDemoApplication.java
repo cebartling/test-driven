262cc3abeabacadd6620a94c44a66f4a543c5ffe
Christopher Bartling <chris.bartling@gmail.com>
2021-07-27 12:40:58 -0500

    Create Spring Boot Webflux TDD demo

---
+++ spring-boot/webflux-tdd-demo/src/main/java/com/pintailconsultingllc/webflux/demo/WebfluxTddDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.webflux.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTddDemoApplication.class, args);
+	}
+
+}
