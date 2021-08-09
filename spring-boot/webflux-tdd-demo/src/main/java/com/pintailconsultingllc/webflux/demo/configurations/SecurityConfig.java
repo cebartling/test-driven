2795c6c982316a148a08d763238102c33a147019
Christopher Bartling <chris.bartling@gmail.com>
2021-08-08 21:34:41 -0500

    Permit all for securityFilterChain

--- spring-boot/webflux-tdd-demo/src/main/java/com/pintailconsultingllc/webflux/demo/configurations/SecurityConfig.java
+++ spring-boot/webflux-tdd-demo/src/main/java/com/pintailconsultingllc/webflux/demo/configurations/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.pintailconsultingllc.webflux.demo.configurations;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -10,13 +9,10 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.csrf().disable();
-//                .authorizeExchange()
-//                .pathMatchers(HttpMethod.POST, "/employees/update").hasRole("ADMIN")
-//                .pathMatchers("/**").permitAll()
-//                .and()
-//                .httpBasic();
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange()
+                .permitAll();
         return http.build();
     }
 }
