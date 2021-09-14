8c5b98b7a46733dbeda1ca26890c5a34ed888375
Christopher Bartling <chris.bartling@gmail.com>
2021-09-14 11:12:46 -0500

    Building out the reactive form

--- angular/example1/src/app/views/profile-view/__tests__/profile-view.component.spec.ts
+++ angular/example1/src/app/views/profile-view/__tests__/profile-view.component.spec.ts
@@ -18,8 +18,26 @@ describe('ProfileViewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('constructor', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should create a FormGroup', () => {
+      expect(component.profileFormGroup).toBeDefined();
+    });
+
+    it('should create firstName form control within the FormGroup', () => {
+      expect(component.profileFormGroup.controls['firstName']).toBeDefined();
+    });
+
+    it('should create lastName form control within the FormGroup', () => {
+      expect(component.profileFormGroup.controls['lastName']).toBeDefined();
+    });
+
+    it('should create username form control within the FormGroup', () => {
+      expect(component.profileFormGroup.controls['username']).toBeDefined();
+    });
   });
 
   describe('template rendering', () => {
--- angular/example1/src/app/views/profile-view/profile-view.component.ts
+++ angular/example1/src/app/views/profile-view/profile-view.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'tdd-example1-profile-view',
   templateUrl: './profile-view.component.html',
-  styleUrls: ['./profile-view.component.scss']
+  styleUrls: ['./profile-view.component.scss'],
 })
 export class ProfileViewComponent implements OnInit {
+  profileFormGroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.profileFormGroup = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      username: new FormControl(''),
+    });
   }
 
+  ngOnInit(): void {}
 }
