34f835ec7508a3e47a00d379f251ad454abb822c
Christopher Bartling <chris.bartling@gmail.com>
2021-10-04 13:30:05 -0500

    Adding Puppeteer to allow headless Chrome testing.

--- angular/example1/karma.conf.js
+++ angular/example1/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -36,7 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     //browsers: ['Chromium'], /* Use this if on Linux. */
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true,
   });
