9e3667e85be577e72cae6644d32e449bacd7b457
Christopher Bartling <chris.bartling@gmail.com>
2021-10-15 14:51:48 -0500

    Pasting things

---
+++ java/introduction-to-junit-5/src/main/java/com/pintailconsultingllc/AssertionDemonstration.java
@@ -0,0 +1,15 @@
+package com.pintailconsultingllc;
+
+public class AssertionDemonstration {
+    public boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
