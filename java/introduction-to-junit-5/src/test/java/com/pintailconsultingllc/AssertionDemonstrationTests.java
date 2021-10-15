ab18392e5141c796d77bdee6f6ba521c83de5bca
Christopher Bartling <chris.bartling@gmail.com>
2021-10-15 14:59:12 -0500

    Finished up on assertAll.

--- java/introduction-to-junit-5/src/test/java/com/pintailconsultingllc/AssertionDemonstrationTests.java
+++ java/introduction-to-junit-5/src/test/java/com/pintailconsultingllc/AssertionDemonstrationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -17,6 +18,11 @@ public class AssertionDemonstrationTests {
     /**
      * Assert that all supplied executables do not throw exceptions. All supplied executables are executed,
      * regardless of success or failure of others.
+     *
+     * The interesting thing about assertAll is that it always checks all of the assertions that are passed
+     * to it, no matter how many fail. If all pass, all is fine. If at least one fails you get a detailed
+     * result of all that went wrong. It is best used for asserting a set of properties that belong
+     * together.
      */
     @Nested
     @DisplayName("assertAll")
@@ -41,8 +47,18 @@ public class AssertionDemonstrationTests {
 
         @Test
         @DisplayName("collection of executables usage")
-        void assert_all_collection_executables() {
+        void assert_all_collection_of_executables() {
             assertAll("isPrime", List.of(
+                    () -> assertTrue(sut.isPrime(2)),
+                    () -> assertFalse(sut.isPrime(4)),
+                    () -> assertFalse(sut.isPrime(2344))
+            ));
+        }
+
+        @Test
+        @DisplayName("stream of executables usage")
+        void assert_all_stream_of_executables() {
+            assertAll("isPrime", Stream.of(
                     () -> assertTrue(sut.isPrime(2)),
                     () -> assertFalse(sut.isPrime(4)),
                     () -> assertFalse(sut.isPrime(2344))
