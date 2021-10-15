65a6f70afe55079811f986dcac9c13be3d74cfb7
Christopher Bartling <chris.bartling@gmail.com>
2021-10-15 13:05:10 -0500

    Created Maven application

---
+++ java/src/main/java/com/pintailconsultingllc/App.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Do something here
+    }
+}
