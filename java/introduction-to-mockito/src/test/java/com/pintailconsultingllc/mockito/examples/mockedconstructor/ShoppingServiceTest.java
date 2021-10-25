876219e08c49dd0b1b2eb3cbc45edf296e1df4e2
Christopher Bartling <chris.bartling@gmail.com>
2021-10-25 12:42:39 -0500

    Refactored MockedConstruction demonstration

--- java/introduction-to-mockito/src/test/java/com/pintailconsultingllc/mockito/examples/mockedconstructor/ShoppingServiceTest.java
+++ java/introduction-to-mockito/src/test/java/com/pintailconsultingllc/mockito/examples/mockedconstructor/ShoppingServiceTest.java
@@ -1,16 +1,17 @@
 package com.pintailconsultingllc.mockito.examples.mockedconstructor;
 
-import com.pintailconsultingllc.mockito.examples.mockedconstructor.ShoppingCart;
-import com.pintailconsultingllc.mockito.examples.mockedconstructor.ShoppingCartItem;
-import com.pintailconsultingllc.mockito.examples.mockedconstructor.ShoppingService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockedConstruction;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Field;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.Mockito.doNothing;
@@ -42,7 +43,7 @@ class ShoppingServiceTest {
 
             @Test
             @DisplayName("verify ShoppingCart.addItem is called on new shopping cart")
-            void DemonstrationTest() {
+            void VerifyAddItemInvocationTest() {
                 final MockedConstruction.MockInitializer<ShoppingCart> mockInitializer = (mock, context) -> {
                     // Stubbing the object instance that was intercepted via the constructor.
                     // Stub normally like any other Mockito mock.
@@ -55,11 +56,39 @@ class ShoppingServiceTest {
                     shoppingService.addItemToShoppingCart(shoppingCartKey, shoppingCartItem);
 
                     // Use the MockedConstruction instance to get a reference to the mocked ShoppingCart instance.
-                    final ShoppingCart mockedShoppingCart = mocked.constructed().get(0);
+                    final ShoppingCart shoppingCartMock = mocked.constructed().get(0);
 
                     // Now verify interactions as normal with a mock object.
-                    verify(mockedShoppingCart).addItem(shoppingCartItem);
+                    verify(shoppingCartMock).addItem(shoppingCartItem);
                 }
+            }
+        }
+
+        @Nested
+        @DisplayName("when shopping cart previously exists")
+        class WhenShoppingCartIsCachedTests {
+
+            @Mock
+            ShoppingCart shoppingCartMock;
+
+            @BeforeEach
+            public void doBeforeEachTest() throws Exception {
+                // Using reflection, get access the shopping cart map and add an entry to it, basically pre-caching
+                // the shopping cart.
+                final Field shoppingCartMapField = ShoppingService.class.getDeclaredField("shoppingCartMap");
+                shoppingCartMapField.setAccessible(true);
+                final Map<UUID, ShoppingCart> shoppingCartMap = (Map<UUID, ShoppingCart>)
+                        shoppingCartMapField.get(shoppingService);
+                shoppingCartMap.put(shoppingCartKey, shoppingCartMock);
+                // Now exercise the SUT.
+                shoppingService.addItemToShoppingCart(shoppingCartKey, shoppingCartItem);
+            }
+
+            @Test
+            @DisplayName("verify ShoppingCart.addItem is called on existing shopping cart")
+            void VerifyAddItemInvocationTest() {
+                // Now verify interactions as normal with a mock object.
+                verify(shoppingCartMock).addItem(shoppingCartItem);
             }
         }
     }
