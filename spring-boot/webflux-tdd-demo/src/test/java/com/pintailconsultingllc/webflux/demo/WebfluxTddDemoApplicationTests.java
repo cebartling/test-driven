08669ce1e6662298633f54f2f88054ff9191ba0a
Christopher Bartling <chris.bartling@gmail.com>
2021-11-12 12:51:46 -0600

    More assertions regarding application context

--- spring-boot/webflux-tdd-demo/src/test/java/com/pintailconsultingllc/webflux/demo/WebfluxTddDemoApplicationTests.java
+++ spring-boot/webflux-tdd-demo/src/test/java/com/pintailconsultingllc/webflux/demo/WebfluxTddDemoApplicationTests.java
@@ -1,17 +1,42 @@
 package com.pintailconsultingllc.webflux.demo;
 
+import com.pintailconsultingllc.webflux.demo.repositories.DepartmentRepository;
+import com.pintailconsultingllc.webflux.demo.repositories.EmployeeRepository;
+import com.pintailconsultingllc.webflux.demo.services.DepartmentService;
+import com.pintailconsultingllc.webflux.demo.services.EmployeeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration")
 @Tag("UnitTest")
 @DisplayName("WebfluxTddDemoApplication unit tests")
 class WebfluxTddDemoApplicationTests {
 
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    DepartmentService departmentService;
+    @Autowired
+    EmployeeService employeeService;
+
     @Test
     @DisplayName("application context should load")
     void contextLoads() {
+        assertAll(
+                () -> assertNotNull(departmentRepository),
+                () -> assertNotNull(employeeRepository),
+                () -> assertNotNull(departmentService),
+                () -> assertNotNull(employeeService)
+        );
     }
 }
