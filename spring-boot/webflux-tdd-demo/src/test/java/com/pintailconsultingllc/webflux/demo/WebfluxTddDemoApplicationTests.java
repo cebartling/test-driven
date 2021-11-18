8fa6bc6fd29b66d12385923cb3262df75c2250cb
Christopher Bartling <christopher_bartling@optum.com>
2021-11-18 07:49:37 -0600

    Minor refactorings

--- spring-boot/webflux-tdd-demo/src/test/java/com/pintailconsultingllc/webflux/demo/WebfluxTddDemoApplicationTests.java
+++ spring-boot/webflux-tdd-demo/src/test/java/com/pintailconsultingllc/webflux/demo/WebfluxTddDemoApplicationTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
