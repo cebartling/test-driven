1e68c08dcfef2195635f46713434f905ad1d1343
Christopher Bartling <chris.bartling@gmail.com>
2021-12-14 14:28:29 -0600

    Creating Java demo application for Testcontainers

---
+++ testcontainers/java-demo/src/test/java/com/pintailconsultingllc/testcontainers/demo/TestcontainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.testcontainers.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
