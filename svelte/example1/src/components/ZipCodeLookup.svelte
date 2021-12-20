a9295c46561ac390a2fc334bd1c004d3f29a9817
Christopher Bartling <chris.bartling@gmail.com>
2021-12-20 12:25:14 -0600

    First Svelte component test.

---
+++ svelte/example1/src/components/ZipCodeLookup.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let zipCode: string = undefined;
+
+  export function handleOnSubmit() {
+    console.log('handleOnSubmit');
+  }
+</script>
+
+<div>
+  <form on:submit={handleOnSubmit}>
+    <div class="">
+      <input type="text">
+    </div>
+    <div class="">
+      <button type="submit">Search</button>
+    </div>
+  </form>
+</div>
+
+<style>
+    /* styles go here */
+</style>
--- svelte/example1/src/components/__tests__/ZipCodeLookup.spec.ts
+++ svelte/example1/src/components/__tests__/ZipCodeLookup.spec.ts
@@ -1,7 +1,14 @@
-import * as ZipCodeLookupServiceExports  from "../../services/ZipCodeLookupService";
+import {render, RenderResult} from '@testing-library/svelte'
+import ZipCodeLookup from "../ZipCodeLookup.svelte";
 
 describe('ZipCodeLookup.svelte component', () => {
-  it('should be true', () => {
-    expect(true).toBeTruthy();
+  let renderedComponent: RenderResult;
+
+  beforeEach(() => {
+    renderedComponent = render(ZipCodeLookup);
+  });
+
+  it('should successfully render the component', () => {
+    expect(renderedComponent).toBeDefined();
   });
 });
