bf222ed80645d33cdf014163438dfb71eff9e17a
Christopher Bartling <chris.bartling@gmail.com>
2021-12-22 17:15:56 -0600

    Added profile editor

---
+++ svelte/example1/src/components/ProfileEditor.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import type {Profile} from '../models/Profile';
+
+  export let profile: Profile;
+
+</script>
+
+<div class="row">
+  <hr/>
+</div>
+<div class="row">
+  <div class="mb-3 col-4">
+    <label for="emailFormControlInput" class="form-label">Email address</label>
+    <input type="email"
+           class="form-control"
+           id="emailFormControlInput"
+           placeholder="name@example.com"
+           bind:value={profile.emailAddress}>
+  </div>
+</div>
+<div class="row">
+  <div class="mb-3 col-4">
+    <label for="givenNameFormControlInput" class="form-label">Given name</label>
+    <input type="text"
+           class="form-control"
+           id="givenNameFormControlInput"
+           bind:value={profile.givenName}>
+  </div>
+</div>
+<div class="row">
+  <div class="mb-3 col-4">
+    <label for="surnameFormControlInput" class="form-label">Surname</label>
+    <input type="text"
+           class="form-control"
+           id="surnameFormControlInput"
+           bind:value={profile.surname}>
+  </div>
+</div>
+<div class="row">
+  <hr/>
+</div>
+<div class="row">
+  <div class="col-6">
+    <button type="button" class="btn btn-success">Save</button>
+  </div>
+</div>
---
+++ svelte/example1/src/models/Profile.ts
@@ -0,0 +1,6 @@
+export interface Profile {
+  id: string;
+  emailAddress: string;
+  givenName: string;
+  surname: string;
+}
