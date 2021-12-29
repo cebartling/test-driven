92ac30525194ba46fc5edb087bc2e8b375147bba
Christopher Bartling <chris.bartling@gmail.com>
2021-12-29 16:17:29 -0600

    Added form validation to the profile editor

--- svelte/example1/src/components/ProfileEditor.svelte
+++ svelte/example1/src/components/ProfileEditor.svelte
@@ -1,57 +1,138 @@
 <script lang="ts">
-  import {ProfileServices} from '../services/ProfileServices';
+  import {createForm} from 'svelte-forms-lib';
+  import * as yup from 'yup';
   import type {Profile} from '../models/Profile';
+  import {ProfileServices} from "../services/ProfileServices";
 
   export let profile: Profile;
 
-  export async function handleOnClickSaveButton() {
-    const response = await ProfileServices.updateProfile(profile);
-    if (!response.ok) {
-      throw new Error(`Error updating profile. Status code: ${response.status}`)
+  const profileSchema = yup.object().shape({
+    emailAddress: yup.string()
+      .required('The email address is a required field!')
+      .email('The email address doesn\'t quite look right!'),
+    givenName: yup.string().required('The given name is a required field!'),
+    surname: yup.string().required('The surname is a required field!'),
+  });
+
+  const {form, handleChange, handleSubmit, isValid, isSubmitting, errors} = createForm({
+    initialValues: {
+      emailAddress: profile.emailAddress,
+      givenName: profile.givenName,
+      surname: profile.surname,
+    },
+    validationSchema: profileSchema,
+    onSubmit: async (values) => {
+      profile.surname = values.surname;
+      profile.givenName = values.givenName;
+      profile.emailAddress = values.emailAddress;
+
+      const response = await ProfileServices.updateProfile(profile);
+      if (!response.ok) {
+        throw new Error(`Error updating profile. Status code: ${response.status}`)
+      }
     }
+  });
+
+  function handleOnClickResetProfileButton() {
+    form.set({
+      emailAddress: profile.emailAddress,
+      givenName: profile.givenName,
+      surname: profile.surname,
+    });
   }
+
 </script>
 
 <div class="row">
   <hr/>
 </div>
-<div class="row">
-  <div class="mb-3 col-4">
-    <label for="emailFormControlInput" class="form-label">Email address</label>
-    <input type="email"
-           class="form-control"
-           id="emailFormControlInput"
-           placeholder="name@example.com"
-           bind:value={profile.emailAddress}>
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="row">
+    <div class="mb-3 col-4">
+      <label for="emailAddressFormControlInput"
+             class="form-label">
+        Email address
+      </label>
+      <input type="email"
+             class="form-control"
+             id="emailAddressFormControlInput"
+             name="emailAddressFormControlInput"
+             placeholder="name@example.com"
+             class:is-invalid={$errors.emailAddress}
+             on:change={handleChange}
+             on:blur={handleChange}
+             bind:value={$form.emailAddress}>
+    </div>
+    <div class="mb-3 col-8 error-message">
+      {#if $errors.emailAddress}{$errors.emailAddress}{/if}
+    </div>
   </div>
-</div>
-<div class="row">
-  <div class="mb-3 col-4">
-    <label for="givenNameFormControlInput" class="form-label">Given name</label>
-    <input type="text"
-           class="form-control"
-           id="givenNameFormControlInput"
-           bind:value={profile.givenName}>
+  <div class="row">
+    <div class="mb-3 col-4">
+      <label for="givenNameFormControlInput"
+             class="form-label">
+        Given name
+      </label>
+      <input type="text"
+             class="form-control"
+             id="givenNameFormControlInput"
+             name="givenNameFormControlInput"
+             class:is-invalid={$errors.givenName}
+             on:change={handleChange}
+             on:blur={handleChange}
+             bind:value={$form.givenName}>
+    </div>
+    <div class="mb-3 col-8 error-message">
+      {#if $errors.givenName}{$errors.givenName}{/if}
+    </div>
   </div>
-</div>
-<div class="row">
-  <div class="mb-3 col-4">
-    <label for="surnameFormControlInput" class="form-label">Surname</label>
-    <input type="text"
-           class="form-control"
-           id="surnameFormControlInput"
-           bind:value={profile.surname}>
+  <div class="row">
+    <div class="mb-3 col-4">
+      <label for="surnameFormControlInput"
+             class="form-label">
+        Surname
+      </label>
+      <input type="text"
+             class="form-control"
+             name="surnameFormControlInput"
+             id="surnameFormControlInput"
+             class:is-invalid={$errors.surname}
+             on:change={handleChange}
+             on:blur={handleChange}
+             bind:value={$form.surname}>
+    </div>
+    <div class="mb-3 col-8 error-message">
+      {#if $errors.surname}{$errors.surname}{/if}
+    </div>
   </div>
-</div>
-<div class="row">
-  <hr/>
-</div>
-<div class="row">
-  <div class="col-6">
-    <button type="button"
-            class="btn btn-success"
-            id="saveProfileButton"
-            on:click={handleOnClickSaveButton}>Save
-    </button>
+  <div class="row">
+    <hr/>
   </div>
-</div>
+  <div class="row">
+    <div class="col-6">
+      <button type="button"
+              on:click={handleOnClickResetProfileButton}
+              class="btn btn-secondary"
+              id="resetProfileButton">
+        Reset form fields
+      </button>
+      <button type="submit"
+              class="btn btn-success"
+              disabled={$isSubmitting}
+              id="saveProfileButton">
+        {#if $isSubmitting}Saving...{:else}Save{/if}
+      </button>
+    </div>
+  </div>
+</form>
+
+
+<style>
+    .error-message {
+        font-size: 0.9rem;
+        font-weight: 700;
+        color: darkred;
+        vertical-align: center;
+        margin-top: 2.6rem;
+    }
+</style>
