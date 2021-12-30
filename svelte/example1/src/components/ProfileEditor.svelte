1f4b85e4a993e9885a864a2fc9edebaf1ae7ce9e
Christopher Bartling <chris.bartling@gmail.com>
2021-12-30 12:13:57 -0600

    Wired up validation errors to UI

--- svelte/example1/src/components/ProfileEditor.svelte
+++ svelte/example1/src/components/ProfileEditor.svelte
@@ -20,6 +20,10 @@
       throw new Error(`Error updating profile. Status code: ${response.status}`)
     }
   }
+
+  export function handleOnClickResetFormButton() {
+    profileForm.reset();
+  }
 </script>
 
 <div class="row">
@@ -30,9 +34,18 @@
     <label for="emailFormControlInput" class="form-label">Email address</label>
     <input type="email"
            class="form-control"
+           class:is-invalid={!$emailAddress.valid}
            id="emailFormControlInput"
            placeholder="name@example.com"
            bind:value={$emailAddress.value}>
+  </div>
+  <div class="mb-3 col-8 error-message">
+    {#if $profileForm.hasError('emailAddress.required')}
+      The email address is a required field!
+    {/if}
+    {#if $profileForm.hasError('emailAddress.not_an_email')}
+      The email address that you provided doesn't look right!
+    {/if}
   </div>
 </div>
 <div class="row">
@@ -40,8 +53,12 @@
     <label for="givenNameFormControlInput" class="form-label">Given name</label>
     <input type="text"
            class="form-control"
+           class:is-invalid={!$givenName.valid}
            id="givenNameFormControlInput"
            bind:value={$givenName.value}>
+  </div>
+  <div class="mb-3 col-8 error-message">
+    {#if $profileForm.hasError('givenName.required')}The given name is a required field!{/if}
   </div>
 </div>
 <div class="row">
@@ -49,8 +66,12 @@
     <label for="surnameFormControlInput" class="form-label">Surname</label>
     <input type="text"
            class="form-control"
+           class:is-invalid={!$surname.valid}
            id="surnameFormControlInput"
            bind:value={$surname.value}>
+  </div>
+  <div class="mb-3 col-8 error-message">
+    {#if $profileForm.hasError('surname.required')}The surname is a required field!{/if}
   </div>
 </div>
 <div class="row">
@@ -59,10 +80,27 @@
 <div class="row">
   <div class="col-6">
     <button type="button"
-            class="btn btn-success"
+            class="btn btn-secondary"
+            id="resetProfileFormButton"
+            on:click={handleOnClickResetFormButton}>
+      Reset form to original values
+    </button>
+    <button type="button"
+            class="btn btn-success ml-2"
             id="saveProfileButton"
             disabled={!$profileForm.valid}
-            on:click={handleOnClickSaveButton}>Save
+            on:click={handleOnClickSaveButton}>
+      Save
     </button>
   </div>
 </div>
+
+
+<style>
+  .error-message {
+      font-size: 0.9rem;
+      font-weight: 600;
+      color: darkred;
+      margin-top: 2.5rem;
+  }
+</style>
