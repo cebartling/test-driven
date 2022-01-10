7ea12dce1f50665121e6eec63d3c958b85056afc
Christopher Bartling <chris.bartling@gmail.com>
2022-01-09 21:51:10 -0600

    Mocking user module ProfileStore with a manual mock

---
+++ svelte/example1/src/stores/__mocks__/ProfileStore.ts
@@ -0,0 +1,14 @@
+import {writable} from 'svelte/store';
+import type {Profile} from '../../models/Profile';
+
+const expectedProfile = {
+  id: "78b6c7b2-6c30-4604-b7cd-56e6cecaae83",
+  emailAddress: "jasper/.shaw@example.com",
+  givenName: "Jasper",
+  surname: "Shawn"
+} as Profile;
+
+export const profileId = writable(expectedProfile.id);
+export const profile = writable(expectedProfile);
+
+export const fetchProfile = jest.fn();
--- svelte/example1/src/views/__tests__/ProfileView.spec.ts
+++ svelte/example1/src/views/__tests__/ProfileView.spec.ts
@@ -1,24 +1,16 @@
-import {get} from 'svelte/store';
 import {cleanup, getByText, render, RenderResult} from '@testing-library/svelte';
-import type {Profile} from '../../models/Profile';
 import ProfileView from '../ProfileView.svelte';
-import {profileId} from "../../stores/ProfileStore";
+import {fetchProfile} from '../../stores/ProfileStore';
+
+// https://jestjs.io/docs/mock-functions#mocking-modules
+// https://jestjs.io/docs/manual-mocks
+// Mock the module using the manual mocks found in '../../stores/__mocks__/ProfileStore'
+jest.mock('../../stores/ProfileStore');
 
 describe('ProfileEditor.svelte component', () => {
   let renderResult: RenderResult;
-  const profile = {
-    id: "78b6c7b2-6c30-4604-b7cd-56e6cecaae83",
-    emailAddress: "jasper.shaw@example.com",
-    givenName: "Jasper",
-    surname: "Shawn"
-  } as Profile;
 
   beforeEach(() => {
-    const response = {
-      ok: true,
-      json: () => Promise.resolve(profile)
-    };
-    global.window.fetch = jest.fn().mockResolvedValue(response);
     renderResult = render(ProfileView);
   });
 
@@ -34,8 +26,9 @@ describe('ProfileEditor.svelte component', () => {
       expect(getByText(/Surname/)).toBeInTheDocument();
     });
 
-    it('should invoke the fetch API to retrieve the profile from the backend REST API', () => {
-      expect(global.window.fetch).toHaveBeenCalledWith(`/api/profiles/${get(profileId)}`, {"method": "GET"});
+    it('should invoke the fetchProfile function', () => {
+      // This is a spy defined in '../../stores/__mocks__/ProfileStore'
+      expect(fetchProfile).toHaveBeenCalled();
     });
   });
 });
