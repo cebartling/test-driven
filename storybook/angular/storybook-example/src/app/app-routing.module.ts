3ba562a2f50a6ffa6cbb8de39f3a170eb511bcf5
Christopher Bartling <chris.bartling@gmail.com>
2022-01-13 15:21:02 -0600

    Welcome view is now displaying

--- storybook/angular/storybook-example/src/app/app-routing.module.ts
+++ storybook/angular/storybook-example/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RaceListingViewComponent} from "./views/race-listing-view/race-listing-view.component";
+import {WelcomeViewComponent} from "./views/welcome-view/welcome-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'races', component: RaceListingViewComponent },
+  // { path: 'race/{}', component: RaceViewComponent },
+  // { path: 'race/{}/results', component: RaceResultsViewComponent },
+  { path: '**', component: WelcomeViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
