00387cb9ca833ac0509ca684dab9ee274f655138
Christopher Bartling <chris.bartling@gmail.com>
2022-01-14 13:12:46 -0600

    Created getRaces method on RaceService

--- storybook/angular/storybook-example/src/app/services/race.service.spec.ts
+++ storybook/angular/storybook-example/src/app/services/race.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
-
-import { RaceService } from './race.service';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Observable} from "rxjs";
+import {RaceService} from './race.service';
+import {Race} from "../types/race";
 
 describe('RaceService', () => {
   let service: RaceService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RaceService],
+    });
     service = TestBed.inject(RaceService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getRaces', () => {
+    let races$: Observable<Race[]>
+    let captured: Race[];
+    let capturedError: Error;
+    const expectedRaces = [] as Race[];
+    const expectedUrl = 'http://localhost:3000/races';
+
+    beforeEach( (done: DoneFn) => {
+      races$ = service.getRaces();
+      races$.subscribe(
+        (data: Race[]) => {
+          captured = data;
+          done();
+        },
+        (error) => {
+          capturedError = error;
+          done();
+        }
+      );
+      httpTestingController.expectOne(expectedUrl).flush(expectedRaces);
+    });
+
+    it('should return the appropriate response', () => {
+      expect(captured).toEqual(expectedRaces);
+    });
+
+    it('should not throw an error', () => {
+      expect(capturedError).toBeUndefined();
+    });
   });
 });
--- storybook/angular/storybook-example/src/app/services/race.service.ts
+++ storybook/angular/storybook-example/src/app/services/race.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Race} from "../types/race";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RaceService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getRaces(): Observable<Race[]> {
+    return this.httpClient.get<Race[]>('http://localhost:3000/races');
+  }
 }
