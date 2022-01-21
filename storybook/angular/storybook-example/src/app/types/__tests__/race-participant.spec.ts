d81959c51973597b70699a1b7eb5e11a6fe9675c
Christopher Bartling <chris.bartling@gmail.com>
2022-01-20 22:58:03 -0600

    Adding method to RaceParticipant

---
+++ storybook/angular/storybook-example/src/app/types/__tests__/race-participant.spec.ts
@@ -0,0 +1,56 @@
+import { RaceParticipant, RaceParticipantImpl } from '../race-participant';
+
+describe('RaceParticipantImpl class', () => {
+  describe('calculateCompletedRaceTimeInSeconds method', () => {
+    describe('when startDateTime and endDateTime are populated', () => {
+      it('should calculate an appropriate value', () => {
+        const raceParticipant = new RaceParticipantImpl({
+          id: '2ab31c4d-d920-4818-812a-b55edf2f8bb8',
+          raceId: 'bb54c76f-3c78-40e3-808b-75dec4986c0e',
+          riderId: '9a46a083-3d8e-451f-aeca-c4c1b0d98951',
+          startedRace: true,
+          finishedRace: true,
+          startDateTime: new Date('2022-01-22T10:02:34'),
+          endDateTime: new Date('2022-01-22T11:47:02'),
+        } as RaceParticipant);
+        expect(raceParticipant.calculateCompletedRaceTimeInSeconds()).toEqual(
+          6268
+        );
+      });
+    });
+
+    describe('when startDateTime is null and endDateTime is populated', () => {
+      it('should return a value of 0', () => {
+        const raceParticipant = new RaceParticipantImpl({
+          id: '2ab31c4d-d920-4818-812a-b55edf2f8bb8',
+          raceId: 'bb54c76f-3c78-40e3-808b-75dec4986c0e',
+          riderId: '9a46a083-3d8e-451f-aeca-c4c1b0d98951',
+          startedRace: true,
+          finishedRace: true,
+          startDateTime: null,
+          endDateTime: new Date('2022-01-22T11:47:02'),
+        } as RaceParticipant);
+        expect(raceParticipant.calculateCompletedRaceTimeInSeconds()).toEqual(
+          0
+        );
+      });
+    });
+
+    describe('when startDateTime is populated and endDateTime is null', () => {
+      it('should return a value of 0', () => {
+        const raceParticipant = new RaceParticipantImpl({
+          id: '2ab31c4d-d920-4818-812a-b55edf2f8bb8',
+          raceId: 'bb54c76f-3c78-40e3-808b-75dec4986c0e',
+          riderId: '9a46a083-3d8e-451f-aeca-c4c1b0d98951',
+          startedRace: true,
+          finishedRace: true,
+          startDateTime: new Date('2022-01-22T10:02:34'),
+          endDateTime: null,
+        } as RaceParticipant);
+        expect(raceParticipant.calculateCompletedRaceTimeInSeconds()).toEqual(
+          0
+        );
+      });
+    });
+  });
+});
--- storybook/angular/storybook-example/src/app/types/race-participant.ts
+++ storybook/angular/storybook-example/src/app/types/race-participant.ts
@@ -1,3 +1,5 @@
+import { differenceInSeconds } from 'date-fns';
+
 export interface RaceParticipant {
   id: string;
   raceId: string;
@@ -6,4 +8,34 @@ export interface RaceParticipant {
   finishedRace: boolean;
   startDateTime: Date | null;
   endDateTime: Date | null;
+
+  calculateCompletedRaceTimeInSeconds(): number;
+}
+
+export class RaceParticipantImpl implements RaceParticipant {
+  id: string;
+  raceId: string;
+  riderId: string;
+  startedRace: boolean;
+  finishedRace: boolean;
+  startDateTime: Date | null;
+  endDateTime: Date | null;
+
+  constructor(raceParticipant: RaceParticipant) {
+    this.id = raceParticipant.id;
+    this.raceId = raceParticipant.raceId;
+    this.riderId = raceParticipant.riderId;
+    this.startedRace = raceParticipant.startedRace;
+    this.finishedRace = raceParticipant.finishedRace;
+    this.startDateTime = raceParticipant.startDateTime;
+    this.endDateTime = raceParticipant.endDateTime;
+  }
+
+  calculateCompletedRaceTimeInSeconds(): number {
+    let result = 0;
+    if (this.startDateTime && this.endDateTime) {
+      result = differenceInSeconds(this.endDateTime, this.startDateTime);
+    }
+    return result;
+  }
 }
