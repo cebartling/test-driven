fc895e97ff6c0da68eb3e8ce3a7ff8306a2dceb6
Christopher Bartling <chris.bartling@gmail.com>
2022-01-20 23:03:11 -0600

    Mapping to RaceParticipantImpl class

--- storybook/angular/storybook-example/src/app/services/__tests__/race-participant.service.spec.ts
+++ storybook/angular/storybook-example/src/app/services/__tests__/race-participant.service.spec.ts
@@ -5,21 +5,24 @@ import {
 } from '@angular/common/http/testing';
 import { Observable } from 'rxjs';
 import { RaceParticipantService } from '../race-participant.service';
-import { RaceParticipant } from '../../types/race-participant';
+import {
+  RaceParticipant,
+  RaceParticipantImpl,
+} from '../../types/race-participant';
 
 describe('RaceParticipantService', () => {
   let service: RaceParticipantService;
   let httpTestingController: HttpTestingController;
-  const raceId = 'bb54c76f-3c78-40e3-808b-75dec4986c0e';
-  const raceParticipant1 = {
-    id: '2ab31c4d-d920-4818-812a-b55edf2f8bb8',
-    raceId: 'bb54c76f-3c78-40e3-808b-75dec4986c0e',
-    riderId: '9a46a083-3d8e-451f-aeca-c4c1b0d98951',
+  const raceId ="bb54c76f-3c78-40e3-808b-75dec4986c0e"';
+  const raceParticipant1 = new RaceParticipantImpl({
+    id:"2ab31c4d-d920-4818-812a-b55edf2f8bb8"',
+    raceId:"bb54c76f-3c78-40e3-808b-75dec4986c0e"',
+    riderId:"9a46a083-3d8e-451f-aeca-c4c1b0d98951"',
     startedRace: true,
     finishedRace: true,
-    startDateTime: new Date('2022-01-22T10:02:34'),
-    endDateTime: new Date('2022-01-22T11:47:02'),
-  } as RaceParticipant;
+    startDateTime: new Date"2022-01-22T10:02:34"'),
+    endDateTime: new Date"2022-01-22T11:47:02"',
+  } as RaceParticipant);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
--- storybook/angular/storybook-example/src/app/services/race-participant.service.ts
+++ storybook/angular/storybook-example/src/app/services/race-participant.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { RaceParticipant } from '../types/race-participant';
+import {
+  RaceParticipant,
+  RaceParticipantImpl,
+} from '../types/race-participant';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +14,14 @@ export class RaceParticipantService {
   constructor(private httpClient: HttpClient) {}
 
   getRaceParticipantsByRace(raceId: string): Observable<RaceParticipant[]> {
-    return this.httpClient.get<RaceParticipant[]>(
-      `/api/raceParticipants?raceId=${raceId}`
-    );
+    return this.httpClient
+      .get<RaceParticipant[]>(`/api/raceParticipants?raceId=${raceId}`)
+      .pipe(
+        map((raceParticipants: RaceParticipant[]) => {
+          return raceParticipants.map((raceParticipant: RaceParticipant) => {
+            return new RaceParticipantImpl(raceParticipant);
+          });
+        })
+      );
   }
 }
