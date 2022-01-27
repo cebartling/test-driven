838feffa2052fa56320f94e66e50018542f433c3
Christopher Bartling <chris.bartling@gmail.com>
2022-01-27 11:28:10 -0600

    Adding welcome view story

--- storybook/angular/storybook-example/src/app/views/welcome-view/welcome-view.component.ts
+++ storybook/angular/storybook-example/src/app/views/welcome-view/welcome-view.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-view',
   templateUrl: './welcome-view.component.html',
   styleUrls: ['./welcome-view.component.css'],
 })
-export class WelcomeViewComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class WelcomeViewComponent {
 }
