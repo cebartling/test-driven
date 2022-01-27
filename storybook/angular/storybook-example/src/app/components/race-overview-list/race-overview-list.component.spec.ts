8877ebba0aa7f0487c01b12cec83957f24e991c9
Christopher Bartling <chris.bartling@gmail.com>
2022-01-27 12:31:52 -0600

    Fixing Jasmine specs

--- storybook/angular/storybook-example/src/app/components/race-overview-list/race-overview-list.component.spec.ts
+++ storybook/angular/storybook-example/src/app/components/race-overview-list/race-overview-list.component.spec.ts
@@ -1,32 +1,27 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { of } from 'rxjs';
-import { RaceOverviewListComponent } from './race-overview-list.component';
-import { races } from '../../../test-data/race-test-data';
-import { RaceService } from '../../services/race.service';
-import { RaceOverviewCardComponent } from '../race-overview-card/race-overview-card.component';
 import { RouterModule } from '@angular/router';
+import { RaceOverviewListComponent } from './race-overview-list.component';
+import { RaceOverviewCardComponent } from '../race-overview-card/race-overview-card.component';
+import { races } from '../../../test-data/race-test-data';
 
 describe('RaceOverviewListComponent', () => {
   let component: RaceOverviewListComponent;
   let fixture: ComponentFixture<RaceOverviewListComponent>;
-  let raceService: RaceService;
 
   beforeEach(
     waitForAsync(async () => {
       await TestBed.configureTestingModule({
         imports: [RouterModule.forRoot([]), HttpClientTestingModule],
         declarations: [RaceOverviewListComponent, RaceOverviewCardComponent],
-        providers: [RaceService],
       }).compileComponents();
-      raceService = TestBed.inject(RaceService);
     })
   );
 
   beforeEach(() => {
-    spyOn(raceService, 'getRaces').and.returnValue(of(races));
     fixture = TestBed.createComponent(RaceOverviewListComponent);
     component = fixture.componentInstance;
+    component.races = races;
     fixture.detectChanges();
   });
 
