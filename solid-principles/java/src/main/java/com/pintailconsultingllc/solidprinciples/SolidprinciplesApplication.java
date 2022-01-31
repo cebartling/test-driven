e834cd15df49c6cb214cc28069b008dc8c330df9
Christopher Bartling <chris.bartling@gmail.com>
2022-01-31 13:45:14 -0600

    New Java project demonstrating SOLID principles

---
+++ solid-principles/java/src/main/java/com/pintailconsultingllc/solidprinciples/SolidprinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.solidprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidprinciplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidprinciplesApplication.class, args);
+	}
+
+}
---
+++ solid-principles/java/src/test/java/com/pintailconsultingllc/solidprinciples/SolidprinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.solidprinciples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidprinciplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
