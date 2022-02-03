cd3d2ee94abd6c2f5de816e4301492feb1706670
Christopher Bartling <chris.bartling@gmail.com>
2022-02-02 21:30:13 -0600

    Checkpoint

--- solid-principles/java/src/main/java/com/pintailconsultingllc/solidprinciples/bad/PasswordUtils.java
+++ solid-principles/java/src/main/java/com/pintailconsultingllc/solidprinciples/bad/PasswordUtils.java
@@ -9,64 +9,45 @@ import java.security.NoSuchAlgorithmException;
  * This class is responsible for hashing passwords and saving hashed passwords somewhere. These are
  * conflicting responsibilities.
  * <p>
- * This class also violates the Open-Closed Principle (OCP). Classes should be open for extension,
- * closed for modification. Adding a new hashing algorithm forces a change on this class.
+ * Adding a new hashing algorithm forces a change on this class, which violates the Open-Closed Principle (OCP).
+ * Classes should be open for extension, but closed for modification.
  */
 public class PasswordUtils {
+
+    public String hashPassword(String password, HashingType hashingType) {
+        String hashedPassword = null;
+        MessageDigest messageDigest = getMessageDigestInstance(hashingType);
+
+        if (messageDigest != null) {
+            byte[] encodedHash = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
+            hashedPassword = bytesToHex(encodedHash);
+        }
+        return hashedPassword;
+    }
+
+
+    public void savePassword(String hashedPassword) {
+        //save to the db
+    }
 
     public void hashAndSavePassword(String password, HashingType hashingType) {
         final String hashedPassword = hashPassword(password, hashingType);
         savePassword(hashedPassword);
     }
 
-    public String hashPassword(String password, HashingType hashingType) {
-        String hashedPassword = null;
-        if (HashingType.SHA3_256.equals(hashingType)) {
-            MessageDigest messageDigest;
-            try {
-                messageDigest = MessageDigest.getInstance("SHA3-256");
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
-            byte[] encodedHash = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
-            hashedPassword = bytesToHex(encodedHash);
-        } else if (HashingType.SHA256.equals(hashingType)) {
-            MessageDigest messageDigest;
-            try {
-                messageDigest = MessageDigest.getInstance("SHA-256");
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
-            byte[] encodedHash = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
-            hashedPassword = bytesToHex(encodedHash);
-        } else if (HashingType.MD5.equals(hashingType)) {
-            MessageDigest messageDigest;
-            try {
-                messageDigest = MessageDigest.getInstance("MD5");
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
-            byte[] encodedHash = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
-            hashedPassword = bytesToHex(encodedHash);
-        }
-        return hashedPassword;
-    }
-
-    public void savePassword(String hashedPassword) {
-        //save to the db
-    }
-
     public String unhashPassword(String hashedPassword, HashingType hashingType) {
         String password = null;
-        if (HashingType.SHA256.equals(hashingType)) {
-            throw new UnsupportedOperationException("Cannot decrypt SHA256 hashing.");
+        if (HashingType.SHA3_256.equals(hashingType)) {
+            throw new UnsupportedOperationException("Cannot decrypt SHA3-256 hashing.");
+        } else if (HashingType.SHA256.equals(hashingType)) {
+            throw new UnsupportedOperationException("Cannot decrypt SHA-256 hashing.");
         } else if (HashingType.MD5.equals(hashingType)) {
             hashedPassword = "hashed with MD5";
         }
         return hashedPassword;
     }
 
-    private static String bytesToHex(byte[] hash) {
+    private String bytesToHex(byte[] hash) {
         StringBuilder hexString = new StringBuilder(2 * hash.length);
         for (int i = 0; i < hash.length; i++) {
             String hex = Integer.toHexString(0xff & hash[i]);
@@ -77,5 +58,21 @@ public class PasswordUtils {
         }
         return hexString.toString();
     }
+
+    private MessageDigest getMessageDigestInstance(HashingType hashingType) {
+        try {
+            if (HashingType.SHA3_256.equals(hashingType)) {
+                return MessageDigest.getInstance("SHA3-256");
+            } else if (HashingType.SHA256.equals(hashingType)) {
+                return MessageDigest.getInstance("SHA-256");
+            } else if (HashingType.MD5.equals(hashingType)) {
+                return MessageDigest.getInstance("MD5");
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
 
 }
