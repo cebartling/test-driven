26d1eb86859fcf10952d3c02df370a187642bf32
Christopher Bartling <chris.bartling@gmail.com>
2022-02-03 12:25:46 -0600

    Initial revision

---
+++ dotnet/tdd-example/tdd-example-tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tdd_example_tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
