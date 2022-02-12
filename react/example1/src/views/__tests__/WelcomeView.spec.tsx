8357a955721ee2449de32cb89f3eb693107eb5be
Christopher Bartling <chris.bartling@gmail.com>
2022-02-12 11:12:26 -0600

    Added Prettier and eslint

--- react/example1/src/views/__tests__/WelcomeView.spec.tsx
+++ react/example1/src/views/__tests__/WelcomeView.spec.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import WelcomeView from '../WelcomeView';
 
 describe('WelcomeView', () => {
-   beforeEach(() => {
-      render(<WelcomeView />);
-   });
+  beforeEach(() => {
+    render(<WelcomeView />);
+  });
 
-   it('should blah', () => {
-      expect(screen.getByTestId('welcome-view-container')).toHaveTextContent('Welcome');
-   });
+  it('should blah', () => {
+    expect(screen.getByTestId('welcome-view-container')).toHaveTextContent('Welcome');
+  });
 });
