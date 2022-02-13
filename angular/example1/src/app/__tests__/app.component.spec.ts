0ada79175c30c22c31e062997f982eb6216687cc
Christopher Bartling <chris.bartling@gmail.com>
2022-02-13 00:06:51 -0600

    Mocking child component

--- angular/example1/src/app/__tests__/app.component.spec.ts
+++ angular/example1/src/app/__tests__/app.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from '../app.component';
 import { NavigationBarComponent } from '../components/navigation-bar/navigation-bar.component';
+import { MockComponent } from 'ng-mocks';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -11,7 +12,7 @@ describe('AppComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         imports: [RouterTestingModule],
-        declarations: [AppComponent, NavigationBarComponent],
+        declarations: [AppComponent, MockComponent(NavigationBarComponent)],
       }).compileComponents();
     })
   );
@@ -28,12 +29,6 @@ describe('AppComponent', () => {
   describe('template rendering', () => {
     it(`should have as title 'example1'`, () => {
       expect(app.title).toEqual('TDD Example 1');
-    });
-
-    it('should render title', () => {
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('.navbar-brand')?.textContent).toContain('Angular TDD Example 1');
     });
   });
 });
