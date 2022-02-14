b02a1de1322342f3f6d0d00394f4ee78d047696a
Christopher Bartling <chris.bartling@gmail.com>
2022-02-14 01:10:01 -0600

    First GraphQL query

--- react/example1/src/main.tsx
+++ react/example1/src/main.tsx
@@ -2,13 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
--- react/example1/src/views/RacesView.tsx
+++ react/example1/src/views/RacesView.tsx
@@ -1,7 +1,16 @@
+import { useQuery } from '@apollo/client';
+import { ALL_RACES_QUERY } from '../graphql/queries/AllRacesQuery';
+
 export default function RacesView() {
+  const { loading, error, data } = useQuery(ALL_RACES_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div data-testid="races-view-container">
       <h1>Races</h1>
+      {JSON.stringify(data)}
     </div>
   );
 }
