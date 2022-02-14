5b219dbaf9c35aa805db1a78ca81b2221c86206f
Christopher Bartling <chris.bartling@gmail.com>
2022-02-13 22:39:05 -0600

    Wiring up a GraphQL server.

---
+++ react/example1/graphql-db.js
@@ -0,0 +1,19 @@
+module.exports = {
+  races: [
+    {
+      id: '78111bc7-903c-417d-8ddf-688575d51d73',
+      name: 'Seeley Hill Big Fat 2022',
+      raceDate: new Date(2022, 1, 14),
+    },
+    {
+      id: '6d9933a9-2b8a-4119-88ab-0ab381463181',
+      name: 'Birkie Fat Tire Festival 2022',
+      raceDate: new Date(2022, 3, 16),
+    },
+    {
+      id: 'c0c655ae-8470-4f31-ab31-21f3c9adfcec',
+      name: 'Fat Forest 14 2022',
+      raceDate: new Date(2022, 2, 12),
+    },
+  ],
+};
