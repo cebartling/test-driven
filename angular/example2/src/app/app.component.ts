5ea619de436ea94a86006a7f2d266b53a1b5d08b
Christopher Bartling <chris.bartling@gmail.com>
2022-02-22 21:07:47 -0600

    Wiring in jQuery.

--- angular/example2/src/app/app.component.ts
+++ angular/example2/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    isJQueryWorking: string = '';
+
+    ngOnInit(): void {
+        // eslint-disable-next-line no-undef
+        $(document).ready(() => {
+            this.isJQueryWorking = 'Jquery is working!!!';
+        });
+    }
+}
