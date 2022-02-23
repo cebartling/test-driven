59729099ac5bbe95568119b2c44e3e21d1f2fd1d
Christopher Bartling <chris.bartling@gmail.com>
2022-02-22 22:32:20 -0600

    First test demonstrating spying on $.fn.

--- angular/example2/src/app/app.component.spec.ts
+++ angular/example2/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import * as $ from 'jquery';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
@@ -16,12 +17,14 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    // it('should render title', () => {
-    //     const fixture = TestBed.createComponent(AppComponent);
-    //     fixture.detectChanges();
-    //     const compiled = fixture.nativeElement as HTMLElement;
-    //     expect(compiled.querySelector('.content span')?.textContent).toContain(
-    //         'example2 app is running!',
-    //     );
-    // });
+    it('should call document ready', () => {
+        spyOn($.fn, 'ready').withArgs(jasmine.any(Function)).and.callThrough();
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        expect($.fn.ready).toHaveBeenCalledWith(jasmine.any(Function));
+        // const compiled = fixture.nativeElement as HTMLElement;
+        // expect(compiled.querySelector('h1')?.textContent).toContain(
+        //     'Jquery is working!!!',
+        // );
+    });
 });
