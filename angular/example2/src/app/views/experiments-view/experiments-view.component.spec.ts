b26126342fe5492a27c0ec649d342003bb998044
Christopher Bartling <chris.bartling@gmail.com>
2022-02-24 13:47:14 -0600

    First test!

--- angular/example2/src/app/views/experiments-view/experiments-view.component.spec.ts
+++ angular/example2/src/app/views/experiments-view/experiments-view.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as $ from 'jquery';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ExperimentsViewComponent } from './experiments-view.component';
 
 describe('ExperimentsViewComponent', () => {
-  let component: ExperimentsViewComponent;
-  let fixture: ComponentFixture<ExperimentsViewComponent>;
+    let component: ExperimentsViewComponent;
+    let fixture: ComponentFixture<ExperimentsViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ExperimentsViewComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(
+        waitForAsync(async () => {
+            await TestBed.configureTestingModule({
+                declarations: [ExperimentsViewComponent],
+            }).compileComponents();
+        }),
+    );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ExperimentsViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ExperimentsViewComponent);
+        component = fixture.componentInstance;
+        // fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('onClickGradientButton function', () => {
+        beforeEach(() => {
+            spyOn($.fn, 'css').and.callThrough();
+            component.onClickSaveButton();
+        });
+
+        it('should set the background color to red for the content update section', () => {
+            // @ts-ignore
+            expect($.fn.css).toHaveBeenCalledWith('background-color', 'red');
+        });
+    });
 });
--- angular/example2/src/app/views/experiments-view/experiments-view.component.ts
+++ angular/example2/src/app/views/experiments-view/experiments-view.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-experiments-view',
-  templateUrl: './experiments-view.component.html',
-  styleUrls: ['./experiments-view.component.css']
+    selector: 'app-experiments-view',
+    templateUrl: './experiments-view.component.html',
+    styleUrls: ['./experiments-view.component.css'],
 })
-export class ExperimentsViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ExperimentsViewComponent {
+    onClickSaveButton() {
+        $('div#content-update-section').css('background-color', 'red');
+        // eslint-disable-next-line no-undef
+        // setTimeout(function () {
+        //     $('div#content-update-section').css('background-color', 'white');
+        // }, 1200);
+    }
 }
