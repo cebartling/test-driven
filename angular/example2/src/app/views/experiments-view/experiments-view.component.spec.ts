8bcfb5b0ee5adcb301ad9c72c3c94f3cd41eb91c
Christopher Bartling <chris.bartling@gmail.com>
2022-02-28 12:55:00 -0600

    More tests.

--- angular/example2/src/app/views/experiments-view/experiments-view.component.spec.ts
+++ angular/example2/src/app/views/experiments-view/experiments-view.component.spec.ts
@@ -1,6 +1,7 @@
 import * as $ from 'jquery';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ExperimentsViewComponent } from './experiments-view.component';
+import JQuery from 'jquery';
 
 describe('ExperimentsViewComponent', () => {
     let component: ExperimentsViewComponent;
@@ -25,14 +26,27 @@ describe('ExperimentsViewComponent', () => {
     });
 
     describe('onClickGradientButton function', () => {
+        let selector: JQuery<HTMLElement>;
+
         beforeEach(() => {
-            spyOn($.fn, 'css').and.callThrough();
+            selector = {
+                css: (propertyName, propertyValue) => {},
+            } as JQuery<HTMLElement>;
+            spyOn($.fn, 'find')
+                .withArgs('div#content-update-section')
+                .and.returnValue(selector);
+            spyOn(selector, 'css').and.callThrough();
             component.onClickSaveButton();
         });
 
+        it('should find the content update section div via jQuery', () => {
+            expect($.fn.find).toHaveBeenCalledWith(
+                'div#content-update-section',
+            );
+        });
+
         it('should set the background color to red for the content update section', () => {
-            // @ts-ignore
-            expect($.fn.css).toHaveBeenCalledWith('background-color', 'red');
+            expect(selector.css).toHaveBeenCalled();
         });
     });
 });
